3ffd7c39c4fc752f801be1ff350c0293a3a91f66
MatiCorsi <matiasleonel_corsi@hotmail.com>
2024-04-04 17:19:00 -0300

    new changes

--- Desafio2/apps.js
+++ Desafio2/apps.js
@@ -1,76 +1,102 @@
-import {promises as fs} from "fs"
+const fs = require('fs');
 
 class ProductManager {
-    constructor(){
-        this.patch = "./products.txt"
+    constructor(filePath) {
+    this.filePath = "./product.json";
+    this.products = [];
+
+    }
+
+    loadProducts() {
+    try {
+        const data = fs.readFileSync(this.filePath, 'utf8')
+        this.products = JSON.parse(data);
+        console.log("Products: ", data)
+    } catch (error) {
         this.products = []
-}
+        console.log("Error loading database", error)
+    }
+    }
+    saveProducts() {
+    const data = JSON.stringify(this.products, null, 2);
+    try {
+        fs.writeFileSync(this.filePath, data);
+        console.log("Product added")
+    } catch (error) {
+        console.log("Error when adding product.", error)
+    }
+    }
+    addProduct(title, description, price, thumbnail, code, stock) {
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+        console.log("Todos los campos son obligatorios.");
+        return;
+    }
 
-static id = 0
+    const codeEncotrado = this.products.find((id) => id.code === code)
+    if (codeEncotrado) {
+        console.log('The code already exists')
+        return
+    }
 
-addProduct = async (title, description, price, thumbnail, code, stock) => {
-
-    ProductManager.id++
-
-    let newProduct = {
-        id: ProductManager.id,
+    const id = this.products.length + 1;
+    const product = {
+        id,
         title,
         description,
         price,
         thumbnail,
         code,
         stock
+    };
+    this.products.push(product);
+    this.saveProducts();
+    }
+    getProducts() {
+        return this.products;
     }
 
-    this.products.push(newProduct)
-    await fs.writeFile(this.patch, JSON.stringify(this.products))
-}
+    getProductsById(id) {
+        const product = this.products.find(product => product.id === id);
+        if(!product){
+            console.log("Not found");
+            return;
+        }
+        return product;
+    }
 
-readProducts = async () => {
-    let answer = await fs.writeFile(this.patch, "utf-8")
-    return JSON.parse(answer)
-}
-getProducts = async () =>{
-    let answer2 = await this.readProducts()
-    return console.log(answer2)
-}
-getProductsById = async (id) => {
-    let answer3 = await this.readProducts()
-    if(!answer3.find(product => product.id === id)){
-        console.log("Product not found")
-    } else {
-        console.log(answer3.find(product => product.id === id))
+    updateProduct(id, updatedFields) {
+    const index = this.products.findIndex(product => product.id === id);
+    if (index !== -1) {
+        this.products[index] = { ...this.products[index], ...updatedFields };
+        this.saveProducts();
+        return true;
+    }
+    return false;
+    }
+
+    deleteProduct(id) {
+    const index = this.products.findIndex(product => product.id === id)
+    if (index !== -1) {
+        this.products.splice(index,1);
+        this.saveProducts();
+        return true;
+    }
+    return false;
     }
 }
-deleteProductsById = async () => {
-    let answer3 = await this.readProducts()
-    let productFilter = answer3.filter(products => products.id !=id)
-    await fs.writeFile(this.patch, JSON.stringify(productFilter))
-    console.log("Product delete")
-}
 
-updateProducts = async (id, ...product) => {
-    await this.deleteProductsById(id)
-    let oldProduct = await this.readProducts()
-    let modifiedProduct =  [
-        {id, ...product}, ... oldProduct]
-        await fs.writeFile(this.patch, JSON.stringify(modifiedProduct))
-}
-}
+const manager = new ProductManager();
 
-const products = new ProductManager
-products.addProduct("Titulo1", "Description1", 1000, "imagen1", "abc111", 5)
-products.addProduct("Titulo2", "Description2", 500, "imagen2", "abc222", 6)
-products.addProduct("Titulo3", "Description3", 250, "imagen3", "abc333", 4)
+manager.addProduct("Auriculares Gmer", "Mejor auricular", 1000, "image1", "A002", 10);
+manager.addProduct("Mouse Gamer", "Mejor mouse", 1500, "image2", "B001", 7);
+manager.addProduct("Monitor Gamer", "Mejor monitor 144hz", 2000, "image3", "C002", 5);
+manager.addProduct("Teclado gamer", "Mejor teclado", 1700, "image4", "D001", 7);
 
-products.getProductsById()
-products.deleteProductsById()
-products.updateProducts({
-    id: 4,
-    title: "Titulo4",
-    description: "Descripcion4",
-    price: 135,
-    thumbnail: "imagen4",
-    code: "abc444",
-    stock: 4,
-})
+
+console.log(manager.getProducts());
+
+manager.updateProduct(1, { price: 1200 });
+console.log(manager.getProducts());
+
+manager.deleteProduct(2);
+console.log(manager.getProducts());
